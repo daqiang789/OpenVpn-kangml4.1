317c976ee104e418b8bef102c771d70aeb072267
daqiang789 <91552725+daqiang789@users.noreply.github.com>
2023-08-25 14:34:08 +0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,6 @@
+clear
+cd /var/www/html/
+wget -q https://raw.githubusercontent.com/daqiang789/OpenVpn-kangml4.1/master/kangml-update.zip
+unzip -o kangml-update.zip
+chmod 777 -R ./
+rm -rf kangml-update.zip
